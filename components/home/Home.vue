1668a02252fd1ce3f0d7a824a36ebb62ede5c5fd
MikeFranco <mfranco_98@yahoo.com>
2019-05-04 19:19:38 -0500

    some css added to home component

--- components/home/Home.vue
+++ components/home/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div>
-      <h1>FAM</h1>
+    <div class="logo">
+      <img src="~/assets/logos/fam.jpeg">
     </div>
     <section class="container" @click="goToStudent" >
         <section class="card">
@@ -12,7 +12,7 @@
     </section>
     <section class="container2" @click="goToEmployee">
       <section class="card">
-        <div> <h1> Profesores </h1> </div>
+        <div> <h1 align="center"> Profesores </h1> </div>
       </section>
     </section>
   </div>
@@ -43,18 +43,24 @@ export default {
   left: 25%;
   display: flex;
   text-align: center;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 50px;
 }
 
 .container2 {
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
   width: 15%;
-  height: 8%;
+  height: 7%;
   position: absolute;
   top: 40%;
   left: 60%;
   display: flex;
   text-align: center;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 50px;
 }
 
 .container:hover {
@@ -64,8 +70,15 @@ export default {
 .container2:hover {
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
 }
+
 .card {
   padding: 2px 16px;
   text-align: center;
+}
+
+.logo{
+  position: absolute;
+  top: 0%;
+  left: 37%;
 }
 </style>

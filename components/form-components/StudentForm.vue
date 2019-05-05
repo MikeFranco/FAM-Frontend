36fcfe479fe77d165e38eea08a28f78839948301
MikeFranco <mfranco_98@yahoo.com>
2019-05-04 22:38:07 -0500

    login for student added

--- components/form-components/StudentForm.vue
+++ components/form-components/StudentForm.vue
@@ -1,6 +1,28 @@
 <template>
   <section>
-    <h1>Hola</h1>
+    <body>
+    <div class="banner">
+      <video autoplay muted loop src="./b.mp4"></video>
+      <div class="content">
+        <h1>Form</h1>
+        <form class="" action="index.html" method="post">
+
+          <label for="">Name</label>
+          <input placeholder="Name" type="text" name="" value="">
+
+          <label for="">Last Name</label>
+          <input placeholder="Last Name" type="text" name="" value="">
+
+          <label for="">Ocuppation</label>
+          <input placeholder="Occupation" type="text" name="" value="">
+
+          <label for="">Entry</label>
+          <input placeholder="Entry" type="text" name="" value="">
+
+        </form>
+      </div>
+  </div>
+  </body>
   </section>
 </template>
 
@@ -11,5 +33,49 @@ export default {
 </script>
 
 <style>
+
+body{
+  padding: 0;
+  margin:0 ;
+  font-family: 'Poppins'
+}
+.banner{
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+.banner video{
+  position: absolute;
+  top:0;
+  left: 0;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+}
+
+.banner .content{
+  position: relative;
+  z-index: 1;
+  text-align: center;
+}
+
+.content{
+  margin: 1em;
+  width: 33%;
+  height: 40%;
+  border: double #070707;
+  border-radius: 30px;
+  background-color:rgba(239,239,239,0.4);
+  box-shadow: 1px 2px 4px rgba(0, 0, 0, .5);
+}
+
+label{
+  display: block;
+}
 
 </style>
